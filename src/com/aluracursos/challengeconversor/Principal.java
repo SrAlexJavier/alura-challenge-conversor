048273a3e9981f2830883f38bcb88f99ec8d9dbe
Sr Alex <sr.alexjavier@gmail.com>
2026-03-01 18:50:32 -0500

    feat: show the interactive view to users

--- src/com/aluracursos/challengeconversor/Principal.java
+++ src/com/aluracursos/challengeconversor/Principal.java
@@ -2,27 +2,49 @@ package com.aluracursos.challengeconversor;
 
 import com.aluracursos.challengeconversor.menu.ConsolePrinter;
 import com.aluracursos.challengeconversor.model.CurrencyPair;
+import com.aluracursos.challengeconversor.service.CurrencyConverter;
 import com.aluracursos.challengeconversor.utils.Config;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         List<CurrencyPair> pairs = Config.getCurrencyPairs();
         ConsolePrinter console = new ConsolePrinter();
+        CurrencyConverter converter = new CurrencyConverter();
+        int lastOption = pairs.size() + 1;
 
         while(true) {
             console.printOptions(pairs);
-            System.out.println("Seleccione una opción (0 para salir)");
+            System.out.println(lastOption + ") Salir");
+            System.out.println("Elija una opción valida:");
+            System.out.println("**********************************************\n");
             int opcion = entrada.nextInt();
 
-            if (opcion == 0) {
+            if (opcion == lastOption) {
                 System.out.println("Saliendo del programa ");
                 break;
             }
+
+            if (opcion >= 1 && opcion <= pairs.size()) {
+                CurrencyPair selectedPair = pairs.get(opcion - 1);
+
+                System.out.print("\nIngrese la cantidad a convertir: \n");
+                double amount = entrada.nextDouble();
+
+                try {
+                    CurrencyConverter.ConversionData result = converter.convert(selectedPair, amount);
+                    result.displayResult();
+                } catch (Exception e) {
+                    System.out.println("Error al realizar la conversión: " + e.getMessage());
+                }
+            } else {
+                System.out.println("Opción inválida. Intente nuevamente.\n");
+            }
         }
+
+        entrada.close();
     }
 }
--- src/com/aluracursos/challengeconversor/utils/Config.java
+++ src/com/aluracursos/challengeconversor/utils/Config.java
@@ -8,8 +8,12 @@ public class Config {
     public static List<CurrencyPair> getCurrencyPairs() {
         return List.of(
                 new CurrencyPair("USD", "PEN"),
-                new CurrencyPair("PEN", "USD")
-        );
+                new CurrencyPair("PEN", "USD"),
+                new CurrencyPair("USD", "EUR"),
+                new CurrencyPair("EUR", "USD"),
+                new CurrencyPair("USD", "GBP"),
+                new CurrencyPair("GBP", "USD")
+                );
     };
     public static final String API_KEY = "93484caea9a62211adf867e6";
 }
